942141c83d985124a5b13a7a4efb7944dc4c7d95
aidris823 <pokeanand@gmail.com>
2019-01-18 01:38:56 -0500

    ok

--- basic_server.c
+++ basic_server.c
@@ -13,16 +13,27 @@ int main() {
   int to_client;
   int from_client;
 
+  
   while (1){
     //fork();
     from_client = server_handshake( &to_client );
     char * steve = calloc(100,1);
-    while(read(from_client,steve,100)){
-	printf("Client says: %s",steve);
+    mkfifo("Luigi",0644);
+    from_client = open("Luigi",O_RDONLY);
+    if (fork()){
+      close(from_client);
+      remove("Luigi");
+    }
+    else{
+      
+      while(read(from_client,steve,100)){
+	printf("Client PID: %d",getpid());
 	char * appendix = "[This was understood by the server.]";
 	strcat(steve,appendix);
 	write(to_client,steve,100);
+      }
     }
   }
+ 
 }
    
